7fc491717c6be55d7c9c02a41abc81ad3a2ef75b
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-03-24 21:53:58 -0300

    stall improved

--- supercub_utilities.lua
+++ supercub_utilities.lua
@@ -599,7 +599,7 @@ function supercub.flightstep(self)
     end
 
     --is an stall, force a recover
-    if self._angle_of_attack > 3.5 and climb_rate < -3.5 then
+    if longit_speed < (supercub.min_speed / 2) and climb_rate < -3.5 and is_flying then
         self._elevator_angle = 0
         self._angle_of_attack = -1
         newpitch = math.rad(self._angle_of_attack)
